e03ab81e89418b5fa8badc09e6fccd9541f5c863
WB-Jang <eeariorie@gmail.com>
2026-01-12 22:11:48 +0900

    src/data 폴더 정리

--- src/data/data_augmentation_generator.py
+++
@@ -1,9 +0,0 @@
-from sdv.single_table import GaussianCopulaSynthesizer
-import pandas as pd
-
-def synthetic_data_generator(path = './data/raw/', file_nm = 'synthetic_data.csv'):
-  file_path = path+file
-  synthesizer = GaussianCopulaSynthesizer.load('distribution_model.pkl')
-  synthetic_data = synthesizer.sample(num_rows=10000)
-  synthetic_data.to_csv(file_path,index=False, encoding='utf-8-sig')
-  
