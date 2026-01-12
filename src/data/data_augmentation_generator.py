274b563e301c32e3757273daa1571d26e6677cad
WB-Jang <eeariorie@gmail.com>
2026-01-12 17:24:02 +0900

    Create synthetic data generator function
    Added a function to generate synthetic data and save it to a specified path.

--- src/data/data_augmentation_generator.py
+++ src/data/data_augmentation_generator.py
@@ -1,6 +1,9 @@
 from sdv.single_table import GaussianCopulaSynthesizer
 import pandas as pd
-synthesizer = GaussianCopulaSynthesizer.load('./distribution_model.pkl')
 
-synthetic_data = synthesizer.sample(num_rows=10000)
-synthetic_data.to_csv('./synthetic_data.csv',index=False, encoding='utf-8-sig')
+def synthetic_data_generator(path = './data/raw/', file_nm = 'synthetic_data.csv'):
+  file_path = path+file
+  synthesizer = GaussianCopulaSynthesizer.load('distribution_model.pkl')
+  synthetic_data = synthesizer.sample(num_rows=10000)
+  synthetic_data.to_csv(file_path,index=False, encoding='utf-8-sig')
+  
